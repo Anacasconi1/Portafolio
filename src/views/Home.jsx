c8314ac6ce6d50cf2d568720ed2819e12896f80b
Anacasconi1 <sabriicasconii@gmail.com>
2024-08-25 00:05:08 -0300

    Responsive try

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -21,7 +21,8 @@ export const Home = () => {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        strokeWidth="1.5"
+                        strokeWidth="2"
+                        width="30"
                         stroke="#96765a"
                         className="contact-icon"
                     >
